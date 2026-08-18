79995d5732bd9f3f28a13d58b5e135e883a0c24c
Michael-Boat-2 <michael.anokyeboateng@gmail.com>
2026-08-18 03:48:07 +0200

    comments removed

--- Lab3/PolicyIteration.py
+++ Lab3/PolicyIteration.py
@@ -3,13 +3,6 @@ from GridWorld import gridWorld, actionLeft, actionDown, actionRight, actionUp
 
 def environment_step(current_pos,action):
 
-    moves = {
-        "Up": (-1,0),
-        "Down": (1,0),
-        "Left": (0,-1),
-        "Right": (0,1)
-    }
-
     pos = [current_pos[0], current_pos[1]]
 
     if action == "Up":
@@ -21,29 +14,19 @@ def environment_step(current_pos,action):
     elif action == "Right":
         pos, grid, success = actionRight(pos, gridWorld)
 
-
+    
     next_pos = pos
     reward = -1
-
-    # dir_row, dir_col = moves[action]
-    # r,c = current_pos[0], current_pos[1]
-
-    # next_row, next_col = r + dir_row, c + dir_col
-
-    # if((next_row < 0 or next_row > 4) or
-    #     (next_col < 0 or next_col > 4)):
-
-    #      next_row, next_col = r,c
-
-
-    # next_pos = [next_row, next_col]
-    # reward = -1
 
     #top left [0,0] is goal state
     terminated = (next_pos == [0,0])
 
 
     return (next_pos, reward, terminated)
+
+
+
+
 
 
 
